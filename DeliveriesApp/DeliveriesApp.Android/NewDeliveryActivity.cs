16ffad6757ef3b85162dbd8e3434cc2493ccfb9f
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-02-02 14:27:30 +0100

    nach Kapitel 161

--- DeliveriesApp/DeliveriesApp.Android/NewDeliveryActivity.cs
+++ DeliveriesApp/DeliveriesApp.Android/NewDeliveryActivity.cs
@@ -6,6 +6,8 @@ using System.Text;
 using Android.App;
 using Android.Content;
 using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -15,13 +17,41 @@ using DeliveriesApp.Model;
 namespace DeliveriesApp.Droid
 {
     [Activity(Label = "NewDeliveryActivity")]
-    public class NewDeliveryActivity : Activity, IOnMapReadyCallback
+    public class NewDeliveryActivity : Activity, IOnMapReadyCallback, ILocationListener
     {
         Button SaveButton;
         EditText packageNameEditText;
         MapFragment mapFragment;
+        double latitude;
+        double longitude;
+        LocationManager locationManager;
+
+        public void OnLocationChanged(Location location)
+        {
+            latitude = location.Latitude;
+            longitude = location.Longitude;
+            mapFragment.GetMapAsync(this);
+        }
 
         public void OnMapReady(GoogleMap googleMap)
+        {
+            MarkerOptions marker = new MarkerOptions();
+            marker.SetPosition(new LatLng(latitude, longitude));
+            marker.SetTitle("Your location");
+            googleMap.AddMarker(marker);
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
         {
             
         }
@@ -37,9 +67,29 @@ namespace DeliveriesApp.Droid
             SaveButton = FindViewById<Button>(Resource.Id.SaveButton);
             packageNameEditText = FindViewById<EditText>(Resource.Id.packageNameEditText);
             mapFragment = FragmentManager.FindFragmentById<MapFragment>(Resource.Id.mapFragment);
-            mapFragment.GetMapAsync(this);
+            //mapFragment.GetMapAsync(this);
 
             SaveButton.Click += SaveButton_Click;
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            locationManager = GetSystemService(Context.LocationService) as LocationManager;
+            string provider = LocationManager.GpsProvider;
+
+            if (locationManager.IsProviderEnabled(provider))
+            {
+                locationManager.RequestLocationUpdates(provider, 5000, 1000, this);
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            locationManager.RemoveUpdates(this);
         }
 
         private async void SaveButton_Click(object sender, EventArgs e)
