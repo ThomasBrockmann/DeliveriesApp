538805b96beb3d857eb24c2028783d55b9342c16
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-02-25 15:26:01 +0100

    nach Kapitel 160

--- DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
+++ DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
@@ -1,13 +1,23 @@
-﻿using Foundation;
+﻿using DeliveriesApp.Model;
+using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveredViewController : UITableViewController
     {
+        List<Delivery> delivered;
         public DeliveredViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public async override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            delivered = await Delivery.GetDelivered();
         }
     }
 }
--- DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
+++ DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
@@ -1,13 +1,22 @@
-﻿using Foundation;
+﻿using DeliveriesApp.Model;
+using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveriesViewController : UITableViewController
     {
+        List<Delivery> deliveries;
         public DeliveriesViewController (IntPtr handle) : base (handle)
         {
+        }
+        public async override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            deliveries = await Delivery.GetDeliveries();
         }
     }
 }
