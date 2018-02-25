af10ff8fe96187d01f7f3a36b5edf7ea33c082c1
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-02-25 17:56:51 +0100

    nach Kapitel 168

--- DeliveriesApp/DeliveriesApp.iOS/NewDeliveryViewController.designer.cs
+++ DeliveriesApp/DeliveriesApp.iOS/NewDeliveryViewController.designer.cs
@@ -16,7 +16,23 @@ namespace DeliveriesApp.iOS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel deliverToLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        MapKit.MKMapView destinationMapView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        MapKit.MKMapView originMapView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UITextField packeNameTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel pickUpLabel { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
@@ -24,9 +40,29 @@ namespace DeliveriesApp.iOS
 
         void ReleaseDesignerOutlets ()
         {
+            if (deliverToLabel != null) {
+                deliverToLabel.Dispose ();
+                deliverToLabel = null;
+            }
+
+            if (destinationMapView != null) {
+                destinationMapView.Dispose ();
+                destinationMapView = null;
+            }
+
+            if (originMapView != null) {
+                originMapView.Dispose ();
+                originMapView = null;
+            }
+
             if (packeNameTextField != null) {
                 packeNameTextField.Dispose ();
                 packeNameTextField = null;
+            }
+
+            if (pickUpLabel != null) {
+                pickUpLabel.Dispose ();
+                pickUpLabel = null;
             }
 
             if (saveBarButtonItem != null) {
