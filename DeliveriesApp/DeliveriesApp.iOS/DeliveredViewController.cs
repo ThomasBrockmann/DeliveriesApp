3d5d97168cea3b563c09b69a67fe9617f0732239
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-02-25 16:15:34 +0100

    nach Kapitel 161

--- DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
+++ DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
@@ -8,16 +8,47 @@ namespace DeliveriesApp.iOS
 {
     public partial class DeliveredViewController : UITableViewController
     {
-        List<Delivery> delivered;
+        List<Delivery> delivered = new List<Delivery>();
         public DeliveredViewController (IntPtr handle) : base (handle)
         {
         }
-
         public async override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
             delivered = await Delivery.GetDelivered();
+            TableView.ReloadData();
         }
+
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return delivered.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("deliveredCell");
+
+            var delivery = delivered[indexPath.Row];
+            cell.TextLabel.Text = delivery.Name??"<null>";
+            switch (delivery.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Waiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "In delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "delivered";
+                    break;
+                default:
+                    cell.DetailTextLabel.Text = "unknown status";
+                    break;
+            }
+            return cell;
+        }
+
     }
 }
--- DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
+++ DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
@@ -8,7 +8,7 @@ namespace DeliveriesApp.iOS
 {
     public partial class DeliveriesViewController : UITableViewController
     {
-        List<Delivery> deliveries;
+        List<Delivery> deliveries = new List<Delivery>();
         public DeliveriesViewController (IntPtr handle) : base (handle)
         {
         }
@@ -16,7 +16,42 @@ namespace DeliveriesApp.iOS
         {
             base.ViewDidLoad();
 
+            //deliveries = await Delivery.GetDeliveries();
+            //TableView.ReloadData();
+        }
+
+        public async override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
             deliveries = await Delivery.GetDeliveries();
+            TableView.ReloadData();
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return deliveries.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("deliveryCell");
+
+            var delivery = deliveries[indexPath.Row];
+            cell.TextLabel.Text = delivery.Name;
+            switch (delivery.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Waiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "In delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "delivered";
+                    break;
+            }
+            return cell;
         }
     }
 }
