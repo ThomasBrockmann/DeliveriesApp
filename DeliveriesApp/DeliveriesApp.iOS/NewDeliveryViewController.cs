7be79a81389e180a25507e062357048112273ad3
Thomas Brockmann <T.Brockmann@von-schaewen.de>
2018-02-25 18:44:19 +0100

    Delivery App Final State

--- DeliveriesApp/DeliveriesApp.iOS/NewDeliveryViewController.cs
+++ DeliveriesApp/DeliveriesApp.iOS/NewDeliveryViewController.cs
@@ -1,5 +1,7 @@
-﻿using DeliveriesApp.Model;
+﻿using CoreLocation;
+using DeliveriesApp.Model;
 using Foundation;
+using MapKit;
 using System;
 using UIKit;
 
@@ -7,6 +9,7 @@ namespace DeliveriesApp.iOS
 {
     public partial class NewDeliveryViewController : UIViewController
     {
+        CLLocationManager locationManager;
         public NewDeliveryViewController (IntPtr handle) : base (handle)
         {
         }
@@ -15,20 +18,64 @@ namespace DeliveriesApp.iOS
         {
             base.ViewDidLoad();
 
+            locationManager = new CLLocationManager();
+            locationManager.RequestWhenInUseAuthorization();
+            originMapView.ShowsUserLocation = true;
+            destinationMapView.ShowsUserLocation = true;
+
+            originMapView.DidUpdateUserLocation += OriginMapView_DidUpdateUserLocation;
+            destinationMapView.DidUpdateUserLocation += DestinationMapView_DidUpdateUserLocation;
+
             saveBarButtonItem.Clicked += SaveBarButtonItem_Clicked;
+        }
+
+        private void DestinationMapView_DidUpdateUserLocation(object sender, MapKit.MKUserLocationEventArgs e)
+        {
+            if (destinationMapView.UserLocation != null)
+            {
+                var coordinates = destinationMapView.UserLocation.Coordinate;
+                var span = new MKCoordinateSpan(0.15, 0.15);
+                destinationMapView.Region = new MKCoordinateRegion(coordinates, span);
+
+                destinationMapView.RemoveAnnotations();
+                destinationMapView.AddAnnotation(new MKPointAnnotation()
+                {
+                    Coordinate = coordinates,
+                    Title = "Your location"
+                });
+            }
+        }
+
+        private void OriginMapView_DidUpdateUserLocation(object sender, MapKit.MKUserLocationEventArgs e)
+        {
+            if(originMapView.UserLocation != null)
+            {
+                var coordinates = originMapView.UserLocation.Coordinate;
+                var span = new MKCoordinateSpan(0.15, 0.15);
+                originMapView.Region = new MKCoordinateRegion(coordinates, span);
+
+                originMapView.RemoveAnnotations();
+                originMapView.AddAnnotation(new MKPointAnnotation()
+                {
+                    Coordinate = coordinates,
+                    Title = "Your location"
+                });
+            }
         }
 
         private async void SaveBarButtonItem_Clicked(object sender, EventArgs e)
         {
+            var origin = originMapView.CenterCoordinate;
+            var destination = destinationMapView.CenterCoordinate;
+
             Delivery delivery = new Delivery()
             {
                 Name = packeNameTextField.Text,
                 Status = 0,
-                OriginLatitude = 0.4,
-                OriginLongitude = 0.4,
-                DestinationLatitude = 0.42,
-                DestinationLongitude = 0.42
-
+                OriginLatitude = origin.Latitude,
+                OriginLongitude = origin.Longitude,
+                DestinationLatitude = destination.Latitude,
+                DestinationLongitude = destination.Longitude
             };
             await Delivery.InsertDelivery(delivery);
         }
